1ee0c63c616f72d23256a734e22d817b923507e2
Juanjo Bazán <jjbazan@gmail.com>
2021-11-04 13:01:49 +0100

    print success messages

--- check_result.rb
+++ check_result.rb
@@ -6,6 +6,7 @@ else
   paper_pdf_path = File.dirname(paper_md_path)+"/paper.pdf"
   if File.exist?(paper_pdf_path)
     system("echo '::set-output name=paper_pdf_path::#{paper_pdf_path}'")
+    system("echo 'Success! PDF file generated at: #{paper_pdf_path}'")
   else
     raise "   !! ERROR: Failed to generate PDF file"
   end
@@ -13,6 +14,7 @@ else
   paper_jats_path = File.dirname(paper_md_path)+"/paper.jats"
   if File.exist?(paper_jats_path)
     system("echo '::set-output name=paper_jats_path::#{paper_jats_path}'")
+    system("echo 'Success! JATS file generated at: #{paper_jats_path}'")
   else
     raise "   !! ERROR: Failed to generate JATS file"
   end
