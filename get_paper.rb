1e40c81ea387828ecd314b19179ef5a90bec3949
Juanjo Bazán <jjbazan@gmail.com>
2021-11-04 12:50:46 +0100

    get paper

---
+++ get_paper.rb
@@ -0,0 +1,19 @@
+require "theoj"
+
+issue_id = ENV["ISSUE_ID"]
+repo_url = ENV["REPO_URL"]
+repo_branch = ENV["PAPER_BRANCH"]
+journal_alias = ENV['JOURNAL_ALIAS']
+
+journal = Theoj::Journal.new(Theoj::JOURNALS_DATA[journal_alias.to_sym])
+issue = Theoj::ReviewIssue.new(journal.data[:reviews_repository], issue_id)
+issue.paper = Paper.from_repo(repo_url, repo_branch)
+submission = Theoj::Submission.new(journal, issue, issue.paper)
+
+paper_path = issue.paper.paper_path
+
+if paper_path.nil?
+  raise "   !! ERROR: Paper file not found"
+else
+  system("echo '::set-output name=paper_file_path::#{paper_path}'")
+end
